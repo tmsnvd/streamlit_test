ebc354d975c65fc36b5eb7d3029aa8f07118aea8
tomas.neverdauskas <tomas.neverdauskas@devbridge.com>
2020-11-26 16:11:34 +0200

    changes

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from transformers import pipeline
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 ner = pipeline("ner", grouped_entities=True)
 
 st.title('Named Entity Recognition')
@@ -9,4 +18,4 @@ st.header('API by TN')
 sentence = st.text_input('Input your sentence here:') 
 
 if sentence:
-    st.write(output = ner(sequence))
+    st.write((sentence))
